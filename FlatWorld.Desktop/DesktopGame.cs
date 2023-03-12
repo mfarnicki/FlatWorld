d46f2efab1a3447c831cacee2b6b7df2f2978db6
mfarnicki <mfarnicki@hotmail.com>
2023-03-12 14:41:21 +0100

    Flat shapes

--- FlatWorld.Desktop/DesktopGame.cs
+++ FlatWorld.Desktop/DesktopGame.cs
@@ -1,4 +1,6 @@
-﻿using FlatWorld.Engine.Graphics;
+﻿using System;
+using FlatWorld.Engine.Graphics;
+using FlatWorld.Engine.Input;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -11,9 +13,12 @@ public class DesktopGame : Game
     private const int ScreenHeight = 720;
 
     private GraphicsDeviceManager graphics;
-    private Sprites sprites;
-    private Screen screen;
+    private FlatSprites sprites;
+    private FlatScreen screen;
+    private FlatShapes shapes;
     private Texture2D texture;
+
+    private int x = 32;
 
     public DesktopGame()
     {
@@ -31,8 +36,10 @@ public class DesktopGame : Game
         this.graphics.PreferredBackBufferHeight = DesktopGame.ScreenHeight;
         this.graphics.ApplyChanges();
 
-        this.sprites = new Sprites(this);
-        this.screen = new Screen(this, DesktopGame.ScreenWidth, DesktopGame.ScreenHeight);
+        this.sprites = new FlatSprites(this);
+        this.screen = new FlatScreen(this, DesktopGame.ScreenWidth, DesktopGame.ScreenHeight);
+        this.shapes = new FlatShapes(this);
+
         base.Initialize();
     }
 
@@ -43,9 +50,18 @@ public class DesktopGame : Game
 
     protected override void Update(GameTime gameTime)
     {
-        if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+        FlatKeyboard keyboard = FlatKeyboard.Instance;
+        keyboard.Update();
+
+        if (keyboard.IsKeyClicked(Keys.Escape))
         {
             this.Exit();
+        }
+
+        if (keyboard.IsKeyClicked(Keys.Right))
+        {
+            x += 32;
+            Console.WriteLine("Right key clicked.");
         }
 
         // TODO: Add your update logic here
@@ -59,9 +75,15 @@ public class DesktopGame : Game
         Viewport vp = this.GraphicsDevice.Viewport;
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        this.sprites.Begin();
-        this.sprites.Draw(this.texture, null, new Rectangle(32, 32, 512, 256), Color.White);
-        this.sprites.End();
+        // this.sprites.Begin();
+        // this.sprites.Draw(this.texture, null, new Rectangle(x, 32, 512, 256), Color.White);
+        // this.sprites.End();
+
+        this.shapes.Begin();
+        this.shapes.DrawRectangle(100 + x, 100, 64, 64, Color.Red);
+        this.shapes.DrawRectangle(200 - x, 200, 64, 64, Color.Blue);
+        this.shapes.DrawRectangle(300 + x, 300, 64 + 2 * x, 64, Color.Green);
+        this.shapes.End();
 
         this.screen.UnSet();
         this.screen.Present(this.sprites);
